da3b5df48c7386e701b041330d0c848255fd2078
Benjamin DiGregorio <ben@digregor.io>
2021-01-22 22:17:49 -0800

    Import object pooler scripts and wire them up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,51 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float xRange = 20;
     public GameObject projectilePrefab;
-    public float horizontalInput;
-    private Transform _playerTransform;
-    private float _nextAllowedProjectileTs = 0f;
 
-    private const float Speed = 20f;
-    private const float XRange = 15f;
-    private const float ProjectileDelay = 0.2f;
-    private const float ProjectileOffset = 1.25f;
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
+        // Check for left and right bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // Player movement left to right
         horizontalInput = Input.GetAxis("Horizontal");
-        (_playerTransform = transform).Translate(horizontalInput * Speed * Time.deltaTime * Vector3.right);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        var playerPosition = _playerTransform.position;
-        KeepPlayerInBounds(playerPosition);
-        SpawnProjectiles(playerPosition);
-    }
 
-    private void KeepPlayerInBounds(Vector3 playerPosition)
-    {
-        // Place player at horizontal bounds if they have moved outside of it
-        if (playerPosition.x < -XRange)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(-XRange, playerPosition.y, playerPosition.z);
-        }
-        if (playerPosition.x > XRange)
-        {
-            transform.position = new Vector3(XRange, playerPosition.y, playerPosition.z);
-        }
-    }
+            // No longer necessary to Instantiate prefabs
+            // Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 
-    private void SpawnProjectiles(Vector3 playerPosition)
-    {
-        // Create a projectile if the spacebar is pressed and the delay has passsed
-        if (Time.time > _nextAllowedProjectileTs && Input.GetKey(KeyCode.Space))
-        {
-            var projectilePosition = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + ProjectileOffset);
-            Instantiate(projectilePrefab, projectilePosition, projectilePrefab.transform.rotation);
-            _nextAllowedProjectileTs = Time.time + ProjectileDelay;
+            // Get an object object from the pool
+            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
+            if (pooledProjectile != null)
+            {
+                pooledProjectile.SetActive(true); // activate it
+                pooledProjectile.transform.position = transform.position; // position it at player
+            }
         }
+
+
+
     }
 }
